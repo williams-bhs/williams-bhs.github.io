16740ca143a7386cfb628cf5815970bafc0b23b9
jedediyah <jedediyah@gmail.com>
2025-06-29 14:04:44 -0400

    Combinatorics

--- notes.js
+++ notes.js
@@ -1,4 +1,7 @@
 
+// I can't remember for the life of me why I called this "notes.js". 
+// Make it make sense. 
+
 function toggleSolution(button) {
     const solutionDiv = button.nextElementSibling;
     const isVisible = solutionDiv.style.display === "block";
