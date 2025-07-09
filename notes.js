70cd61a0a72fd89c71d03520f958bd0048b31839
jedediyah <jedediyah@gmail.com>
2025-07-09 13:19:04 -0400

    Feedback

--- notes.js
+++ notes.js
@@ -1,10 +1,14 @@
 
-// I can't remember for the life of me why I called this "notes.js". 
-// Make it make sense. 
-
 function toggleSolution(button) {
     const solutionDiv = button.nextElementSibling;
     const isVisible = solutionDiv.style.display === "block";
     solutionDiv.style.display = isVisible ? "none" : "block";
     button.textContent = isVisible ? "Show Solution" : "Hide Solution";
+}
+
+function toggleAdvanced(button) {
+    const solutionDiv = button.nextElementSibling;
+    const isVisible = solutionDiv.style.display === "block";
+    solutionDiv.style.display = isVisible ? "none" : "block";
+    // button.textContent = isVisible ? "Show Solution" : "Hide Solution";
 }
